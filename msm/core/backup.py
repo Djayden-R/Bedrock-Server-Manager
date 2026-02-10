6cd36528ac3e70b3b020399f4ac389eed3e8de78
Djayden-R <6953@rgomiddelharnis.nl>
2026-02-10 13:08:34 +0100

    remove bug causing multiple zips that override themselves

--- msm/core/backup.py
+++ msm/core/backup.py
@@ -29,18 +29,20 @@ def generate_zip(cfg: Config, backup_name: str) -> Optional[Path]:
     """Generate a zip file from the backup directories"""
     if cfg.backup_directories and cfg.path_base:
         with tempfile.TemporaryDirectory() as temp_dir:
+            
+            # Move all directories into temporary folder
             for directory in cfg.backup_directories:
                 directory = Path(directory)
-                # Get name and new location of directory that is getting backed up
                 temp_dest = os.path.join(temp_dir, directory.name)
-                # Copy directory to destination
                 shutil.copytree(directory, temp_dest)
-                # Determine a safe output directory for the archive
-                archive_base = Path(cfg.path_base) / backup_name
-                shutil.make_archive(str(archive_base), "zip", root_dir=temp_dir)
-                archive_path = archive_base.with_suffix('.zip')
-                log.info(f"Zip file generated: {archive_path}")
-                return archive_path
+
+            # Make zip file in main folder
+            backup_location = Path(os.path.join(cfg.path_base, backup_name))
+            shutil.make_archive(str(backup_location), "zip", root_dir=temp_dir)
+            backup_path = backup_location.with_suffix('.zip')
+
+            log.info(f"Zip file generated: {backup_path}")
+            return backup_path
     else:
         log.warning("There are no backup directories or base path defined")
         return None
@@ -50,12 +52,13 @@ def backup_drive(cfg: Config, backup_symlink: Path, folder: str, filename: str):
     t_beginning = monotonic()
     # Create the backup folder
     subprocess.run(["rclone", "mkdir", f"{cfg.backup_drive_name}{folder}"])
-
-    log.info("Starting upload to drive...")
+    log.info(f"Made folder for backup: {folder}")
 
     # Get the actual path from the symlink
     backup_path = os.path.realpath(backup_symlink)
 
+    log.info("Starting upload to drive...")
+    
     # Upload to drive via rclone
     subprocess.run(["rclone", "copyto", backup_path, f"{cfg.backup_drive_name}{folder}/{filename}"])
 
@@ -80,6 +83,8 @@ def update_sym_link(cfg: Config, backup_path: Path):
 
 
 def quick_backup(cfg: Config):
+    log.info("Starting local and hdd backup")
+
     # Check if directories exist and if local backup doesn't, it creates it
     if not cfg.backup_directories:
         raise ValueError("Please add the directories you want to backup to 'directories'")
@@ -98,7 +103,8 @@ def quick_backup(cfg: Config):
                 os.makedirs(backup_folder)
                 log.info(f"Created backup folder for today: '{backup_folder}'")
 
-    temp_backup_path = generate_zip(cfg, backup_name)  # Generate a zip file at a predictable location
+    # Generate a zip file at a predictable location
+    temp_backup_path = generate_zip(cfg, backup_name)
 
     if not temp_backup_path:
         return
@@ -119,10 +125,13 @@ def quick_backup(cfg: Config):
 
 
 def drive_backup(cfg: Config):
+    """Upload the latest backup to an online drive"""
+
     # The backup is from the day before, so this is calculated
     backup_date = datetime.datetime.now() - datetime.timedelta(days=1)
     folder = backup_date.strftime("backup/%y-%m-%d")
     filename = backup_date.strftime("backup_%H-%M-%S.zip")
+
     if cfg.backup_local_path:
         # Get backup zip from latest backup symlink
         latest_backup_path = Path(os.path.join(cfg.backup_local_path, "latest_backup.zip")) 
@@ -131,7 +140,7 @@ def drive_backup(cfg: Config):
         raise ValueError("Local backup is not defined")
 
 
-def main(cfg: Config, type: str = "quick"):  
+def main(cfg: Config, type: str = "quick"):
     "Check if a backup to hard drive or to the cloud is needed"
     if type == "quick":
         quick_backup(cfg)
