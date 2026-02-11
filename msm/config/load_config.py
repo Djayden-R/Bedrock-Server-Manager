9caf86222c43085bbe030ec60f295611f57ffb7b
Djayden-R <6953@rgomiddelharnis.nl>
2026-02-11 17:36:17 +0100

    change names of config variables and make variables required

--- msm/config/load_config.py
+++ msm/config/load_config.py
@@ -2,32 +2,39 @@ import yaml
 from pathlib import Path
 from dataclasses import dataclass
 from typing import Optional, List
-import os
 from ipaddress import IPv4Address, IPv6Address
 import sys
 
 
 @dataclass(frozen=True)
 class Config:
+    ## Required
+
+    # Minecraft Server
+    mc_ip: IPv4Address | IPv6Address
+    mc_port: int
+
+    # Paths
+    path_base: Path
+    
+    ## Optional
+
     # Home Assistant
-    ha_ip: Optional[str] = None
-    ha_token: Optional[str] = None
-    ha_update_entity: Optional[str] = None
-    ha_shutdown_entity: Optional[str] = None
+    mqtt_ip: Optional[str] = None
+    mqtt_port: Optional[int] = None
+    mqtt_username: Optional[str] = None
+    mqtt_password: Optional[str] = None
 
     # Dynu DNS
     dynu_pass: Optional[str] = None
     dynu_domain: Optional[str] = None
 
-    # Minecraft Server
-    mc_ip: Optional[IPv4Address | IPv6Address] = None
-    mc_port: Optional[int] = None
 
     # Backup settings
-    backup_local_path: Optional[str] = None
-    backup_hdd_path: Optional[str] = None
-    backup_drive_name: Optional[str] = None
-    backup_directories: Optional[List[str]] = None
+    backup_local_path: Optional[Path] = None
+    backup_hdd_path: Optional[Path] = None
+    backup_drive_name: Optional[Path] = None
+    backup_directories: Optional[List[Path]] = None
 
     # Timing
     timing_begin_valid: Optional[int] = None
@@ -35,27 +42,55 @@ class Config:
     timing_shutdown: Optional[int] = None
     timing_drive_backup: Optional[int] = None
 
-    # Paths
-    path_base: Optional[str] = None
-
     @classmethod
     def load(cls) -> "Config":
-        program_location = os.path.abspath(sys.executable if getattr(sys, 'frozen', False) else __file__)
-        dir_path = os.path.dirname(program_location)
-        config_location = Path(os.path.join(dir_path, "config.yaml"))
+        # Check if program is bundled
+        program_frozen = getattr(sys, "frozen", False)
 
-        if not config_location.exists():
-            raise FileNotFoundError(f"Config file not found: {config_location}")
+        # Get path where program is run
+        if program_frozen:
+            program_folder_path = Path(sys.executable).resolve().parent
+        else:
+            program_folder_path = Path(__file__).resolve().parent
+        
+        config_path = program_folder_path / "config.yaml"
 
-        data = yaml.safe_load(config_location.read_text())
+        if not config_path.exists():
+            raise FileNotFoundError(f"Config file not found: {config_path}")
+
+
+        data: dict = yaml.safe_load(config_path.read_text())
+
+
+
+        """
+        The code below turns yaml structure like this
+
+        server:
+            ip: 21
+            port: 22
+        backup: true
+        
+        into a flat dictionary like this
+
+        {
+            "server_ip": 21,
+            "server_port": 22,
+            backup: True
+        }
+        
+        This dictionary is then returned as a Config class
+        """
 
         # Flatten nested structure
-        flat_data = {}
+        flat_data: dict = {}
+
         for section, values in data.items():
+            # Section contains nested dictionary
             if isinstance(values, dict):
-                for key, value in values.items():  # type: ignore
+                for key, value in values.items():
                     flat_data[f"{section}_{key}"] = value
             else:
                 flat_data[section] = values
 
-        return cls(**flat_data)  # type: ignore
+        return Config(**flat_data)
