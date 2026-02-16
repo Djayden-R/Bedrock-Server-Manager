fee6b3f4f9f7e52909125800e99e839eeb16bdbc
Djayden-R <6953@rgomiddelharnis.nl>
2026-02-16 19:43:15 +0100

    add build script for executable

---
+++ build tools/compile.sh
@@ -0,0 +1,32 @@
+# Update all programs
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+# Clone repo
+git clone -b dev --single-branch https://github.com/Djayden-R/Bedrock-server-manager.git
+
+# Make a virtual environment
+sudo apt install python3.12-venv -y
+
+python3 -m venv venv
+
+source ./venv/bin/activate
+
+# Install dependencies
+pip install -r Bedrock-server-manager/requirements.txt
+
+pip install pyinstaller
+
+# Make the executable
+pyinstaller --onefile --add-data "setup.sh:." main.py
+
+# Move to home folder
+cp dist/main ~/bsm
+
+# Clean up files
+cd
+
+deactivate
+
+rm -rf Bedrock-server-manager venv
