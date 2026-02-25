c141396723510569b93c4ebaa530a55b3b3d3e0c
Djayden-R <6953@rgomiddelharnis.nl>
2026-02-25 22:39:22 +0100

    streamline code

--- msm/core/backup.py
+++ msm/core/backup.py
@@ -13,7 +13,7 @@ import logging
 log = logging.getLogger("bsm")
 
 
-def generate_file_name(cfg: Config):
+def generate_file_name():
     """Generate a file and foldername based on the date"""
     date = datetime.datetime.now()
 
@@ -27,23 +27,24 @@ def generate_file_name(cfg: Config):
 
 def generate_zip(cfg: Config, backup_name: str) -> Optional[Path]:
     """Generate a zip file from the backup directories"""
-    if cfg.backup_directories:
-        with tempfile.TemporaryDirectory() as temp_dir:
-            for directory in cfg.backup_directories:
-                directory = Path(directory)
-                # Get name and new location of directory that is getting backed up
-                temp_dest = os.path.join(temp_dir, directory.name)
-                # Copy directory to destination
-                shutil.copytree(directory, temp_dest)
-                # Determine a safe output directory for the archive
-                archive_base = Path(cfg.path_base) / backup_name
-                shutil.make_archive(str(archive_base), "zip", root_dir=temp_dir)
-                archive_path = archive_base.with_suffix('.zip')
-                log.info(f"Zip file generated: {archive_path}")
-                return archive_path
-    else:
+
+    if not cfg.backup_directories:
         log.warning("There are no backup directories or base path defined")
         return None
+    
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for directory in cfg.backup_directories:
+            directory = Path(directory)
+            # Get name and new location of directory that is getting backed up
+            temp_dest = os.path.join(temp_dir, directory.name)
+            # Copy directory to destination
+            shutil.copytree(directory, temp_dest)
+            # Determine a safe output directory for the archive
+            archive_base = Path(cfg.path_base) / backup_name
+            shutil.make_archive(str(archive_base), "zip", root_dir=temp_dir)
+            archive_path = archive_base.with_suffix('.zip')
+            log.info(f"Zip file generated: {archive_path}")
+            return archive_path
 
 
 def backup_drive(cfg: Config, backup_symlink: Path, folder: str, filename: str):
@@ -63,59 +64,52 @@ def backup_drive(cfg: Config, backup_symlink: Path, folder: str, filename: str):
 
 
 def update_sym_link(cfg: Config, backup_path: Path):
-    location = cfg.path_base
-
-    if location:
-        symlink = os.path.join(location, "latest_backup.zip")
-
-        # Check if symlink already exists
-        if os.path.islink(symlink):
-            os.unlink(symlink)
-
-        # Save latest backup to a symlink, so it can be accessed later for the drive backup
-        os.symlink(backup_path, symlink)
-        log.info(f"Symlink: '{symlink}' points to '{backup_path}'")
-    else:
+    if not cfg.path_base:
         raise ValueError("Base path is not defined")
+    
+    symlink = cfg.path_base / "latest_backup.zip"
+
+    # Check if symlink already exists and remove it and the last_backup
+    if symlink.exists():
+        last_backup = symlink.resolve()
+        os.remove(last_backup)
+        symlink.unlink()
+
+    # Save latest backup to a symlink, so it can be accessed later for the drive backup
+    os.symlink(backup_path, symlink)
+    log.info(f"Symlink: '{symlink}' points to '{backup_path}'")
 
 
 def quick_backup(cfg: Config):
     # Check if directories exist and if local backup doesn't, it creates it
-    if not cfg.backup_directories:
-        raise ValueError("Please add the directories you want to backup to 'directories'")
-
     if not (cfg.backup_local_path or cfg.backup_hdd_path):
         return
+    elif not cfg.backup_directories:
+        raise ValueError("Please add the directories you want to backup to 'directories'")
 
-    # Generate name and a folder with today's date, if it doesn't exist already from an earlier backup
-    backup_name, folder_name = generate_file_name(cfg)
+    # Generate folder based on today's date and a file name based on the time
+    backup_name, folder_name = generate_file_name()
 
-    # Check if backup locations and folders exist and create them if the don't
-    for backup_location in [cfg.backup_local_path, cfg.backup_local_path]:
+    # Generate a zip file in main (path_base) folder
+    backup_path = generate_zip(cfg, backup_name)
+
+    if not backup_path:
+        return
+
+    for backup_location in (cfg.backup_local_path, cfg.backup_hdd_path):
         if backup_location:
+            # Check if backup folders exists and create it if it doesn't
             backup_folder = os.path.join(backup_location, folder_name)
             if not os.path.exists(backup_folder):
                 os.makedirs(backup_folder)
                 log.info(f"Created backup folder for today: '{backup_folder}'")
+            
+            # Move backup to location
+            backup_folder = os.path.join(backup_location, folder_name)
+            shutil.copy(backup_path, backup_folder)
 
-    temp_backup_path = generate_zip(cfg, backup_name)  # Generate a zip file at a predictable location
-
-    if not temp_backup_path:
-        return
-
-    # Copy the temp backup to the local folder and/or the hdd folder
-    if cfg.backup_local_path:
-        backup_folder = os.path.join(cfg.backup_local_path, folder_name)
-        if not Path(backup_folder).exists():
-            Path(backup_folder).mkdir(parents=True)
-        shutil.copy(temp_backup_path, backup_folder)
-        update_sym_link(cfg, temp_backup_path)  # Update symlink for later backups
-
-    if cfg.backup_hdd_path:
-        backup_folder = os.path.join(cfg.backup_hdd_path, folder_name)
-        if not Path(backup_folder).exists():
-            Path(backup_folder).mkdir(parents=True)
-        shutil.copy(temp_backup_path, backup_folder)
+    # Update last_backup symlink and remove the last backup
+    update_sym_link(cfg, backup_path)  
 
 
 def drive_backup(cfg: Config):
